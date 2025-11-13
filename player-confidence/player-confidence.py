ec977a70b45296c15540b119f01ded6ec9c0ac22
Brian Jing <brianjing1@gmail.com>
2025-11-12 20:20:11 -0800

    removed irrelevant files

--- player-confidence/player-confidence.py
+++ player-confidence/player-confidence.py
@@ -1,3 +1,19 @@
 from nba_api.stats.static import players
 from nba_api.stats.endpoints import shotchartdetail
 from nba_api.stats.static import teams
+import pandas as pd
+
+all_teams = teams.get_teams()
+print(all_teams)
+
+player_dictionary = [player for player in players.get_players() if player["is_active"]]
+print(player_dictionary)
+
+year_2025 = pd.read_csv('/Users/brian/Documents/Python/what-makes-a-clutch-player/shot-chart-mapping/years/2025.csv')
+print(year_2025.columns)
+
+who_made_most = year_2025[['PLAYER_NAME', 'SHOT_MADE_FLAG']].groupby('PLAYER_NAME').sum()
+who_made_most = who_made_most.sort_values(by='SHOT_MADE_FLAG', ascending=False)
+print(who_made_most)
+
+print(len(year_2025['TEAM_NAME'].value_counts()))
