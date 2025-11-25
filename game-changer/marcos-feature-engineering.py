3a0fffdab4864fda1750e4fec439a115b7dcb503
marcoslopez24 <marcosglopez24@gmail.com>
2025-11-24 19:52:23 -0800

    added a specific feature engineering file for myself

---
+++ game-changer/marcos-feature-engineering.py
@@ -0,0 +1,9 @@
+# Marcos:
+# - ['marcos_1']: Opponent strength (defensive and offensive rating of the opponent, with everything amplified in the playoffs)
+# - ['marcos_2']: Away vs home game
+# - ['marcos_3']: Shot clock pressure
+# - ['marcos_4']: Whats on the line (playoffs wise → farther-in games in a series worth more)
+
+#Load the dataframes in and make the four features below:
+
+#...
